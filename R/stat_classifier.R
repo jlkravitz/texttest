204fe5f80146017482330f41400dfc40c284884d
Joshua Kravitz <jlkravitz@gmail.com>
2020-02-02 18:57:20 -0800

    Fix stat classifier

--- R/stat_classifier.R
+++ R/stat_classifier.R
@@ -1,14 +1,17 @@
 #' Classifier test statistic which returns the concordance (C-statistic, AUC, etc.)
 #' of a classifier trained on measurements to predict treatment arm.
+#' @inheritParams stat_summed_mean_difference
 #' @importFrom rlang .data
 #' @export
 stat_classifier <- function(data) {
   data <-
-    spread_measurements(data) %>%
+    data %>%
+    select(trmt, measurement) %>%
+    spread_measurements() %>%
     dplyr::mutate(trmt = as.integer(.data$trmt))
 
   InformationValue::Concordance(
-    data$arm,
+    data$trmt,
     stats::predict(
       suppressWarnings(
         stats::glm(
